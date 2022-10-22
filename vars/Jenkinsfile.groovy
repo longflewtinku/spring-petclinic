b4985777f27a37dbb3f487bce88d8c0c19bbc038
tinku <laxitinku7788@gmail.com>
2022-10-22 17:05:36 +0530

    add-1

---
+++ vars/Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+def call() {
+    pipeline {
+    agent { label 'OPENJDK-JDK' }
+    stages {
+        stage('learning') {
+            steps {
+                git url: 'https://github.com/spring-projects/spring-petclinic.git' , 
+                    branch 'main'
+            }
+        }    
+        stage('Example Build') {
+            steps {
+                sh '/usr/share/maven/bin/mvn package'   
+            }
+        }
+        stage('Junit Results'){
+            steps {
+                Junits '**/surefire-reports/*.xml'
+            }
+        }
+      }
+   }
+}
